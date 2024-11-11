b2b98996446a268ec89b2143b4178b423c04f0ad
A L <lodkin55@yandex.ru>
2024-11-11 16:04:24 +0500

    upd gulp config & package

--- gulpfile.js
+++ gulpfile.js
@@ -5,70 +5,70 @@ const plumber = require('gulp-plumber');
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 const gulpPug = require('gulp-pug');
-const scss = require('gulp-sass')(require('sass'));
+const sass = require('gulp-sass')(require('sass'));
 const uglify = require('gulp-uglify-es').default;
 const del = require('del');
 
 function pug() {
-    return src('src/index.pug')
-        .pipe(gulpPug({ pretty: true }))
-        .pipe(concat('index.html'))
-        .pipe(dest('dist/'))
-        .pipe(browserSync.reload({ stream: true }));
+  return src('src/index.pug')
+    .pipe(gulpPug({ pretty: true }))
+    .pipe(concat('index.html'))
+    .pipe(dest('dist/'))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
-function scssToCss() {
-    return src(['src/index.scss'])
-        .pipe(concat('bundle.css'))
-        .pipe(scss({ outputStyle: 'expanded' }))
-        .pipe(autoprefixer())
-        .pipe(dest('dist/'))
-        .pipe(browserSync.reload({ stream: true }));
+function scss() {
+  return src(['src/index.scss'])
+    .pipe(concat('bundle.css'))
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(autoprefixer())
+    .pipe(dest('dist/'))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 function scripts() {
-    return src('src/**/*.js')
-        .pipe(plumber())
-        .pipe(dest('dist/'))
-        .pipe(browserSync.reload({ stream: true }));
+  return src('src/**/*.js')
+    .pipe(plumber())
+    .pipe(dest('dist/'))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 function images() {
-    return src('src/images/**/*.*')
-        .pipe(plumber())
-        .pipe(dest('dist/images'))
-        .pipe(browserSync.reload({ stream: true }));
+  return src('src/images/**/*.*')
+    .pipe(plumber())
+    .pipe(dest('dist/images'))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 function fonts() {
-    return src('src/fonts/**/*.*', { encoding: false })
-        .pipe(plumber())
-        .pipe(dest('dist/fonts'))
-        .pipe(browserSync.reload({ stream: true }));
+  return src('src/fonts/**/*.*', { encoding: false })
+    .pipe(plumber())
+    .pipe(dest('dist/fonts'))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 function clean() {
-    return del('dist');
+  return del('dist');
 }
 
 function watchFiles() {
-    browserSync.init({
-        server: {
-            baseDir: 'dist/',
-            index: 'index.html',
-        },
-    });
-    watch(['src/**/*.pug'], pug);
-    watch(['src/**/*.scss'], scssToCss);
-    watch(['src/fonts/**/*.*'], fonts);
-    watch(['src/images/**/*.*'], images);
-    watch(['src/**/*.js'], scripts);
+  browserSync.init({
+    server: {
+      baseDir: 'dist/',
+      index: 'index.html',
+    },
+  });
+  watch(['src/**/*.pug'], pug);
+  watch(['src/**/*.scss'], scss);
+  watch(['src/fonts/**/*.*'], fonts);
+  watch(['src/images/**/*.*'], images);
+  watch(['src/**/*.js'], scripts);
 }
 
-const build = series(clean, parallel(pug, scssToCss, fonts, images, scripts));
+const build = series(clean, parallel(pug, scss, fonts, images, scripts));
 
 exports.pug = pug;
-exports.scssToCss = scssToCss;
+exports.scss = scss;
 exports.fonts = fonts;
 exports.scripts = scripts;
 exports.images = images;
